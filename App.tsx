890552c51a1c2bcbd55089b3ea4db48bcf1473c5
ThePandow <vlapinski3@gmail.com>
2023-06-03 00:14:50 +0200

    ✨Clicker game mgl

--- App.tsx
+++ App.tsx
@@ -1,14 +1,53 @@
-import React from 'react';
-import {SafeAreaView, StatusBar, StyleSheet, Text} from 'react-native';
+import React, { useState } from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 
 function App(): JSX.Element {
+  const [point, setPoint] = useState(0);
   return (
     <SafeAreaView>
       <StatusBar barStyle={'light-content'} />
+      <Text>test</Text>
+      <Text style={styles.sectionTitle}>PandowLIVE</Text>
+      <View style={styles.viewBorder}>
+        <Text>Test</Text>
+      </View>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          setPoint(point + 1);
+        }}
+      >
+        <Text style={{ color: 'white' }}>AHHH DU BLEU !!!!!!</Text>
+      </TouchableOpacity>
+      <Text>{point}</Text>
     </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: 'red',
+    padding: 50,
+  },
+
+  sectionTitle: {
+    color: 'red',
+    textDecorationLine: 'underline',
+  },
+
+  viewBorder: {
+    borderColor: 'pink',
+    borderWidth: 3,
+    width: 300,
+  },
+});
 
 export default App;
+
