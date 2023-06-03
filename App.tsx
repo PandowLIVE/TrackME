5b722de34b4eefdbc9a3f464389408cc80ccb8ad
ThePandow <vlapinski3@gmail.com>
2023-06-03 18:07:50 +0200

    🎉Landing page done

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import {
+  Image,
   SafeAreaView,
   StatusBar,
   StyleSheet,
@@ -18,57 +19,57 @@ function App(): JSX.Element {
   }, [point]);
 
   return (
-    <SafeAreaView>
-      <StatusBar barStyle={'light-content'} />
-      <Text>test</Text>
-      <Text style={styles.sectionTitle}>PandowLIVE</Text>
-      <View style={styles.viewBorder}>
-        <Text>Test</Text>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.containerMain}>
+        <Image
+          style={styles.tinyLogo}
+          source={{
+            uri: 'https://pandowlive.fr/assets/img/1.png',
+          }}
+        />
+        <Text style={styles.text}>Best Hide and Seek application</Text>
+        <View style={styles.bottomView}>
+          <Text style={styles.textStyle}>Next</Text>
+        </View>
       </View>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => {
-          setPoint(point + 1);
-        }}
-      >
-        <Text style={{ color: 'white' }}>+1</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => {
-          setPoint(point - 5);
-        }}
-      >
-        <Text style={{ color: 'white' }}>-5</Text>
-      </TouchableOpacity>
-      <Text>Tu as {point} points</Text>
-      <Text>
-        {point > 200
-          ? 'Tu es riche ma gueule !'
-          : 'Va faire la manche sale pauvre !'}
-      </Text>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  button: {
-    backgroundColor: 'red',
-    padding: '5%',
-    fontWeight: 'bold',
+  containerMain: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-
-  sectionTitle: {
-    color: 'red',
-    fontWeight: 'bold',
-    textDecorationLine: 'underline',
+  bottomView: {
+    width: '93%',
+    height: 50,
+    backgroundColor: '#00C27C',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute', //Here is the trick
+    bottom: 0, //Here is the trick
+    borderRadius: 10,
+    margin: 15,
   },
-
-  viewBorder: {
-    borderColor: 'pink',
-    borderWidth: 10,
-    fontWeight: 'bold',
-    width: '25%',
+  textStyle: {
+    color: '#fff',
+    fontSize: 18,
+  },
+  text: {
+    color: '#000000',
+    fontWeight: '900',
+    fontSize: 30,
+    textAlign: 'center',
+  },
+  tinyLogo: {
+    width: 408,
+    height: 408,
+  },
+  logo: {
+    width: 66,
+    height: 58,
   },
 });
 
